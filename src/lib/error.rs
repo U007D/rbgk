aec1f934a7646f63f8dcf063f16dfa3c2f822004
bRad <brad@humanenginuity.com>
2017-11-09 17:38:31 -0800

    implemented example error discriminant tightened warnings in error::Error

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -1,17 +1,20 @@
 use std::fmt;
 
-#[allow(unused_imports)]
 use consts::*;
 
-#[allow(empty_enum)]
 #[derive(Fail, Debug)]
 pub enum Error {
+    MyDiscriminant(#[cause] ::std::io::Error),
 }
 
-#[allow(unreachable_code)]
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", match *self {
+            Error::MyDiscriminant(_) => { MSG_ERR_ARG_VALIDATION },
         })
     }
+}
+
+impl From<::std::io::Error> for Error {
+    fn from(err: ::std::io::Error) -> Self { Error::MyDiscriminant(err) }
 }
