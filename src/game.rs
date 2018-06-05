b610c5992748321e707678ccb6cc87c5f82e0690
Brad Gibson <b2b@humanenginuity.com>
2018-06-05 00:03:30 -0700

    test: new game, 1 pin hit: green

--- src/game.rs
+++ src/game.rs
@@ -9,7 +9,9 @@ impl Game {
     pub fn score(&self, rolls: &[u8]) -> Option<u16> {
         match rolls.len() {
             0 => None,
-            _ => Some(0),
+            _ => Some(rolls.iter()
+                           .map(|v| u16::from(*v))
+                           .sum()),
         }
     }
 }
