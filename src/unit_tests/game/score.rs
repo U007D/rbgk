b118df4ccb6ea1bf031273b5a745e350cba88f1d
Brad Gibson <b2b@humanenginuity.com>
2018-06-05 00:06:02 -0700

    test: new game, 2 pins then 3 pins hit: green test: new game, 2 pins hit by one ball: green

--- src/unit_tests/game/score.rs
+++ src/unit_tests/game/score.rs
@@ -16,6 +16,7 @@ impl Default for TestResult {
 struct Env {
     game: Game,
     result: TestResult,
+    expected_result: TestResult,
 }
 
 impl Env {
@@ -23,6 +24,7 @@ impl Env {
         Self {
             game: Game::new(),
             result: TestResult::Untested,
+            expected_result: TestResult::Untested,
         }
     }
 }
@@ -33,43 +35,57 @@ fn tests() {
         ctx.when("no balls are rolled", |ctx| {
             ctx.before(|env| {
                 env.result = TestResult::Tested(env.game.score(&[]));
+                env.expected_result = TestResult::Tested(None);
             });
 
             ctx.then("there is no score (score is None)", |env| {
-                assert!(env.result == TestResult::Tested(None));
+                assert!(env.result == env.expected_result);
             });
         });
 
         ctx.when("a gutterball is rolled", |ctx| {
             ctx.before(|env| {
                 env.result = TestResult::Tested(env.game.score(&[0]));
+                env.expected_result = TestResult::Tested(Some(0));
             });
 
             ctx.then("the score is 0", |env| {
-                assert!(env.result == TestResult::Tested(Some(0)));
+                assert!(env.result == env.expected_result);
             });
         });
 
         ctx.when("one pin is hit by one ball", |ctx| {
             ctx.before(|env| {
                 env.result = TestResult::Tested(env.game.score(&[1]));
+                env.expected_result = TestResult::Tested(Some(1));
             });
 
             ctx.then("the score is 1", |env| {
-                assert!(env.result == TestResult::Tested(Some(1)));
+                assert!(env.result == env.expected_result);
             });
         });
 
         ctx.when("two pins are hit by one ball", |ctx| {
             ctx.before(|env| {
                 env.result = TestResult::Tested(env.game.score(&[2]));
+                env.expected_result = TestResult::Tested(Some(2));
             });
 
             ctx.then("the score is 2", |env| {
-                assert!(env.result == TestResult::Tested(Some(2)));
+                assert!(env.result == env.expected_result);
             });
         });
 
+        ctx.when("two pins, then three pins are hit", |ctx| {
+            ctx.before(|env| {
+                env.result = TestResult::Tested(env.game.score(&[2, 3]));
+                env.expected_result = TestResult::Tested(Some(5));
+            });
+
+            ctx.then("the score is 5", |env| {
+                assert!(env.result == env.expected_result);
+            });
+        });
     }));
 }
 
