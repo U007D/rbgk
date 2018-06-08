df78164f826de4acea5cf7e2e32ab9b9df214814
bRad <brad@humanenginuity.com>
2018-06-08 04:39:39 -0700

    wip: to array syntax

--- src/game.rs
+++ src/game.rs
@@ -22,33 +22,27 @@ impl Game {
         }
 
         #[allow(integer_arithmetic, indexing_slicing)]
-        fn inner_score<'a, I>(rolls: I, curr_frame: usize) -> Option<u16> where I: Clone +
-                                                                                   Debug +
-                                                                                   Iterator<Item = &'a u8> {
+        fn inner_score(rolls: &[u8], curr_frame: usize) -> Option<u16> {
             if curr_frame > FRAMES_PER_GAME {
                 return None;
             }
 
-            let frame_type = match rolls.clone().next() {
-                Some(&ALL_PINS_HIT) => Frame::Strike,
-                _ => Frame::SpareOrOpen,
-            };
-            let rolls_to_start_of_next_frame = match frame_type {
-                Frame::Strike => STRIKE_ROLLS,
-                Frame::SpareOrOpen => SPARE_OR_OPEN_ROLLS,
+            let (frame_type, rolls_to_start_of_next_frame) = match rolls.get(0) {
+                Some(&ALL_PINS_HIT) => (Frame::Strike, STRIKE_ROLLS),
+                _ => (Frame::SpareOrOpen, SPARE_OR_OPEN_ROLLS),
             };
 
-            Some(rolls.clone()
+            Some(rolls.iter()
                       .take(match frame_type {
                           Frame::Strike => STRIKE_SCORING_ROLLS,
                           Frame::SpareOrOpen => SPARE_OR_OPEN_SCORING_ROLLS,
                       })
                       .map(|&v| u16::from(v))
                       .sum::<u16>()
-                + inner_score(rolls.skip(rolls_to_start_of_next_frame), curr_frame + 1).unwrap_or(0))
+                + inner_score(rolls, curr_frame + 1).unwrap_or(0))
         }
 
-        inner_score(rolls.iter(), 1)
+        inner_score(rolls, 1)
     }
 }
 
