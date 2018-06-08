7641e345d6aa04bad569395bc269d8383729778a
Brad Gibson <b2b@humanenginuity.com>
2018-06-08 05:57:44 -0700

    wip

--- src/game.rs
+++ src/game.rs
@@ -15,16 +15,27 @@ impl Game {
         Self {}
     }
 
-    pub fn score(&self, rolls: &[u8]) -> Option<u16> {
+    pub fn score(&self, rolls: &[u8]) -> u16 {
         enum Frame {
             Strike,
             SpareOrOpen,
         }
 
         #[allow(integer_arithmetic, indexing_slicing)]
-        fn inner_score(rolls: &[u8], curr_frame: usize) -> Option<u16> {
-            if curr_frame > FRAMES_PER_GAME {
-                return None;
+        fn inner_score(rolls: &[u8], curr_frame: usize) -> u16 {
+            match curr_frame > FRAMES_PER_GAME {
+                true => return None,
+                false => rolls.iter()
+                              .take(match rolls.iter()
+                                               .take(2) {
+                                        v if v >= 10 => 3,
+                                        _ => 2,
+                                    })
+                              .map(|&v| u16::from(v))
+                              .sum::<u16>() +
+                         match rolls.get(0) {
+                            Some(v) =>i nner_score()
+            }
             }
 
             let (frame_type, rolls_to_start_of_next_frame) = match rolls.get(0) {
@@ -38,8 +49,11 @@ impl Game {
                           Frame::SpareOrOpen => SPARE_OR_OPEN_SCORING_ROLLS,
                       })
                       .map(|&v| u16::from(v))
-                      .sum::<u16>()
-                + inner_score(rolls, curr_frame + 1).unwrap_or(0))
+                      .sum::<u16>() +
+                match rolls.get(rolls_to_start_of_next_frame..) {
+                    Some(s) => inner_score(s, curr_frame + 1).unwrap_or(0),
+                    None => 0,
+                })
         }
 
         inner_score(rolls, 1)
